fe8216eac0d7deeb89674853764ebde36c58e317
omerakdagg <108828661+omerakdagg@users.noreply.github.com>
2022-11-09 17:07:47 +0300

    Proje dosyası ekle.

---
+++ Kapsul/Program.cs
@@ -0,0 +1,27 @@
+﻿using Kapsul;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kapsul alan = new kapsul();
+            Console.WriteLine("eni gir");
+            int eni = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("boyu gir");
+            
+            int boyu = Convert.ToInt32(Console.ReadLine());
+            alan.BOY = boyu;
+            alan.EN = eni;
+            Console.WriteLine("Alan:" +alan.al());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Kapsul/kapsul.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsul
+{
+    public class kapsul
+    {
+        private int en;
+        private int boy;
+
+
+
+        public int EN
+        {
+
+            get 
+            { 
+                return en; 
+            }
+        
+         set
+        {
+                if (value < 0)
+                {
+                    Console.WriteLine("yanlis deger girdiniz lutfen pozitif sayi giriniz");
+                }
+                 else 
+                 {  
+                    en = value; 
+                 }
+
+            
+        }
+
+
+        }
+        public int BOY
+        {
+            get { return boy; }
+            set 
+            { 
+                if (value < 0)
+                {
+                    boy = 0;
+                }
+                else
+                {
+                    boy = value;
+                }
+            }    
+
+        }
+
+
+
+        public int al()
+        {
+            return BOY + EN;
+        }
+        public int MyProperty { get; set; }
+
+    }
+        
+        
+    
+}
